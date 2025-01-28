ecd5bec7f1f6ceeb979d3a599975fb02df764f43
Emmanuel Pinnick <emmanuelpinnick@gmail.com>
2025-01-28 18:36:25 +0000

    Log in page complete.

--- front_end/note-app/src/pages/login.js
+++ front_end/note-app/src/pages/login.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "../styles/login.css"
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Login() {
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigate = useNavigate();
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    console.log("You have successfully logged in with the following details: ", loginEmail, loginPassword);
+    navigate("/dashboard");
+  }
+
   return (
-    <div>Login</div>
+    <div className='login_container'>
+      <div className='login_card'>
+        <h2>Login</h2>
+        <form onSubmit={submitLogin}>
+          <div className='input_segment'>
+            <label htmlFor='email'>Email</label>
+            <input placeholder='Enter Email' required type='email' id='email' onChange={(e) => setLoginEmail(e.target.value)}/>
+          </div>
+
+          <div className='input_segment'>
+            <label htmlFor='password'>Password</label>
+            <input placeholder='Enter Password' required type='password' id='password' onChange={(e) => setLoginPassword(e.target.value)}/>
+          </div>
+
+          <div className='action'>
+            <button>Login</button>
+            <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
