aec935c50232ce4a850dede8a69b3678c14c9e05
Emmanuel Pinnick <emmanuelpinnick@gmail.com>
2025-02-05 12:47:34 +0000

    Co-authored-by: Edoghotu Gideon Azibaobuom <gideonEdoghotu@users.noreply.github.com> Co-authored-by: AlisuTech <AlisuTech@users.noreply.github.com>

--- back_end/db/db.js
+++ back_end/db/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const connectToDB = async () => {
-    await mongoose.connect(process.env.GIDEONDATABASE)
+    await mongoose.connect(process.env.DATABASE)
+    // mongoose.set("strictQuery", true);
+    console.log("Database is Running")
 }
 
 export default connectToDB;
